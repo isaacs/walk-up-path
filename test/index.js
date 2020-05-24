610d12869656b3b10420aaa56659bd13f0bd6d03
isaacs <i@izs.me>
2020-05-24 14:35:10 -0700

    test

---
+++ test/index.js
@@ -0,0 +1,41 @@
+const requireInject = require('require-inject')
+const {posix, win32} = require('path')
+const path = {
+  mode: posix,
+  dirname: (...args) => path.mode.dirname(...args),
+  resolve: (...args) => path.mode.resolve(...args),
+}
+
+const walkUp = requireInject('../', {path})
+
+const t = require('tap')
+
+t.test('posix', async t => {
+  path.mode = posix
+  t.strictSame([...walkUp('/some/kinda/path')], [
+    '/some/kinda/path',
+    '/some/kinda',
+    '/some',
+    '/',
+  ])
+  t.strictSame([...walkUp('/')], ['/'])
+  t.strictSame([...walkUp('')], [...walkUp(process.cwd())])
+})
+
+t.test('win32', async t => {
+  path.mode = win32
+  t.strictSame([...walkUp('c:\\some\\kinda\\path')], [
+    'c:\\some\\kinda\\path',
+    'c:\\some\\kinda',
+    'c:\\some',
+    'c:\\',
+  ])
+  t.strictSame([...walkUp('c:/some/kinda/path')], [
+    'c:\\some\\kinda\\path',
+    'c:\\some\\kinda',
+    'c:\\some',
+    'c:\\',
+  ])
+  t.strictSame([...walkUp('/')], [path.resolve(process.cwd(), '/')])
+  t.strictSame([...walkUp('')], [...walkUp(process.cwd())])
+})
